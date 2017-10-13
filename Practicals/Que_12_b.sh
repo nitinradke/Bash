f65cbbf2968608fc1318842a97d6532ebc6cdc9b
nitin <nitinradkeirctc@gmail.com>
2017-10-13 16:44:40 +0530

    CORrection done

--- Practicals/Que_12_b.sh
+++ Practicals/Que_12_b.sh
@@ -1,3 +1,4 @@
+#Write a shell script to find the number of files in a directory.
 #!/bin/bash
 echo "Enter directory name"
 read dir
--- Practicals/Que_12_c.sh
+++ Practicals/Que_12_c.sh
@@ -1,3 +1,4 @@
+#Write a shell script to display first ten positive numbers using until loop.
 #!/bin/bash
 n=1
 until [ $n -eq 11 ];do
